6039860deee72febc6fe6e500a1d9a71d4a7321b
SOTA <sota00@outlook.jp>
2022-10-02 17:10:08 -0700

    add actual functions in google strategy

--- packages/server/src/passport-strategy.mw.ts
+++ packages/server/src/passport-strategy.mw.ts
@@ -58,7 +58,8 @@ passport.use(
   new GoogleStrategy(
     // clientIDはクライアントを識別するために使用される
     // callbackURLはアクセス承認をもらった後のリダイレクト先
-    // 3. 1で指定したscopeと共にclientIDとcallbackURLを含んだ認可サーバーのURLにリダイレクトされる
+    // passReqToCallbackは第二引数でreqを使えるようにしている
+    // 3. 1で指定したscopeと共にclientIDとcallbackURLを含み認可サーバーのURLにリダイレクトされる
     // 4. リソースオーナーがアクセスを許可し認可コードを発行する
     // 5. 認可コードをqueryとしてcallbackURLに遷移する（例：localhost:80/google/callback?code=<認可コード>）
     // 6. oauth/loginファイルの"/google/callback"のルーター処理に移動
@@ -66,61 +67,43 @@ passport.use(
       clientID,
       clientSecret,
       callbackURL,
+      passReqToCallback: true,
     },
     // 8. queryに付属する認可コードを認可サーバーに渡してアクセストークンを発行する
     // 9. アクセストークンを提示してリソースサーバーにアクセス
     // 10. scopeで指定した情報を取得する
     // 11. oauthでできることはこれで終わり！
-    (accessToken, refreshToken, profile, cb) => {
-      console.log(accessToken);
-      console.log(profile);
-      cb(null, profile);
+    async (req, _, __, profile, done) => {
+      try {
+        const user = await User.findOne({ where: { googleId: profile.id } });
+
+        if (req.query.state === "login") {
+          if (user) {
+            done(null, user);
+          } else {
+            done(null, false);
+          }
+        }
+
+        if (req.query.state === "signup") {
+          if (user) {
+            done(null, false);
+          } else {
+            const newUser = new User();
+            newUser.name = "No name";
+            newUser.email = "email";
+            newUser.lastAccessedAt = new Date();
+            newUser.googleId = profile.id;
+            await newUser.save();
+            done(null, newUser);
+          }
+        }
+
+        done(null, false);
+      } catch (e) {
+        const error = e as Error;
+        done(error);
+      }
     }
   )
 );
-
-// async (_, __, profile, done) => {
-//   const user = await User.findOne({ where: { googleId: profile.id } });
-
-//   // login
-//   if (user) {
-//     const currentUser: CurrentUser = {
-//       id: user.id,
-//       createdAt: user.createdAt,
-//       updatedAt: user.updatedAt,
-//       name: user.name,
-//       email: user.email,
-//       profileImageUrl: user.profileImageUrl,
-//       lastAccessedAt: user.lastAccessedAt,
-//     };
-//     done(null, currentUser);
-//   }
-
-//   if (!profile.emails) {
-//     // emailが一個もないのはおかしい
-//     throw new Error("Something went wrong");
-//   }
-
-//   // signup
-//   if (!user) {
-//     const newUser = new User();
-//     newUser.name = profile.displayName;
-//     newUser.email = profile.emails[0].value;
-//     newUser.profileImageUrl = "";
-//     newUser.lastAccessedAt = new Date();
-//     newUser.googleId = profile.id;
-
-//     const savedUser = await newUser.save();
-
-//     const currentUser: CurrentUser = {
-//       id: savedUser.id,
-//       createdAt: savedUser.createdAt,
-//       updatedAt: savedUser.updatedAt,
-//       name: savedUser.name,
-//       email: savedUser.email,
-//       profileImageUrl: savedUser.profileImageUrl,
-//       lastAccessedAt: savedUser.lastAccessedAt,
-//     };
-//     return done(null, currentUser);
-//   }
-// }
--- packages/server/src/routers/google.ts
+++ packages/server/src/routers/google.ts
@@ -1,22 +1,38 @@
 import { Router } from "express";
 import passport from "passport";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+const frontendUrl = process.env.FRONTEND_URL;
 const googleRouter = Router();
 
 googleRouter.get(
-  "/google",
+  "/google/login",
   // 1. scopeに使用したい情報を指定する
   // 2. passport-strategyファイルのGoogleStrategyの第一引数に移動
-  passport.authenticate("google", { scope: ["email", "profile"] })
+  passport.authenticate("google", {
+    scope: ["email", "profile"],
+    state: "login",
+  })
+);
+
+googleRouter.get(
+  "/google/signup",
+  passport.authenticate("google", {
+    scope: ["email", "profile"],
+    state: "signup",
+  })
 );
 
 googleRouter.get(
   "/google/callback",
   // 7. 処理失敗時のリダイレクト先を指定する
   // 8. passport-strategyファイルのGoogleStrategyの第二引数に移動
-  passport.authenticate("google", { failureRedirect: "/login" }),
-  (req) => {
-    console.log(req.user);
+  passport.authenticate("google", { failureRedirect: `${frontendUrl}/failed` }),
+  (_, res) => {
+    console.log("Success");
+    res.redirect(`${frontendUrl}/signup`);
   }
 );
 
