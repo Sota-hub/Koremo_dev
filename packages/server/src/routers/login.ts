a22a1c6cffafea0a57bf0cf61c4f3da0f1ddb0dc
SOTA <sota00@outlook.jp>
2023-01-30 00:02:03 -0800

    12:for fix the problem deployed server not deserialize

--- packages/server/src/routers/login.ts
+++ packages/server/src/routers/login.ts
@@ -7,18 +7,25 @@ dotenv.config();
 const frontendUrl = process.env.FRONTEND_URL as string;
 const loginRouter = Router();
 
-loginRouter.post(
-  "/login",
-  passport.authenticate("local"),
-  (_, res) => {
-    try {
-      // console.log(req.user);
-      res.redirect(`${frontendUrl}/home`);
-    } catch (e) {
-      const error = e as Error;
-      res.status(400).send({ error: { message: error.message } });
-    }
-  }
-);
+// loginRouter.post(
+//   "/login",
+//   passport.authenticate("local"),
+//   (req, res) => {
+//     try {
+//       console.log(req.cookies);
+//       console.log(req.user);
+//       res.redirect(`${frontendUrl}/home`);
+//     } catch (e) {
+//       const error = e as Error;
+//       res.status(400).send({ error: { message: error.message } });
+//     }
+//   }
+// );
+
+loginRouter.post('/login', 
+  passport.authenticate('local', { failureRedirect: '/login' }),
+  function(_, res) {
+    res.redirect(`${frontendUrl}/home`);
+  });
 
 export default loginRouter;
